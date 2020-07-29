8bc8f0b6750bb33eba6f56849d5653e8121568a6
Edward Bai <bybeye@gmail.com>
2020-07-29 14:42:15 +1000

    finish test

--- src/main/java/demo/service/UserServiceImpl.java
+++ src/main/java/demo/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package demo.service;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,9 @@ import demo.entity.User;
  *
  */
 public class UserServiceImpl implements UserService {
+	
+	private final static Logger logger = Logger.getLogger(UserServiceImpl.class.getName());
+
 
 	@Autowired
 	private UserRepository userRepository;
@@ -28,9 +33,9 @@ public class UserServiceImpl implements UserService {
 	 */
 	public int getTotalNumbersForName(String name) {
 		List<User> list = userRepository.getUsers();
-		System.out.println("user total size:" + list.size()+", " + list);
+		logger.log(Level.FINE, "user total size:" + list.size()+", " + list);
 		List<User> resultList = list.stream().filter(user -> name.equals(user.getName())).collect(Collectors.toList());
-		System.out.println("user results:" + resultList.size() + ", result list:" + resultList);
+		logger.log(Level.FINE, "user results:" + resultList.size() + ", result list:" + resultList);
 		return resultList.size();
 	}
 
